e3716fcecb32f96ee5f23e6711a6e98d38a85de5
김동균 <wooheuy5752@nate.com>
2023-09-16 13:47:45 +0900

    Feat(IntroducePage): 상단 텍스트 추가

--- src/pages/IntroducePage/IntroducePage.js
+++ src/pages/IntroducePage/IntroducePage.js
@@ -22,6 +22,18 @@ function IntroducePage(){
   return(
     <>
       <Banner title="Our Team" subtitle="Home / Team"/>
+
+      <S.TitleBox>
+        <div>
+          <S.Title>What We Do</S.Title>
+          <S.Description>우리 서비스는 어려운 ESG개념을 보편화하기위하여 출발하였습니다. ESG가 어렵기보다는 이제 친숙한 ESG의 사회적 보편화를 위하여 노력합니다.</S.Description>
+        </div>
+        <div>
+          <S.Title>The ultimate goal</S.Title>
+          <S.Description>소비자가 기업의 ESG 경향을 파악, 분석하고 맞춤 소비를 할 수 있게 돕습니다. 습득·학습·공유의 3단계로 가치를 창출하려 합니다.</S.Description>
+        </div>
+      </S.TitleBox>
+
       <S.Profile>
         {
           teamId.map((i, j)=>{
--- src/styles/IntroducePage/IntroducePage.style.js
+++ src/styles/IntroducePage/IntroducePage.style.js
@@ -1,6 +1,36 @@
 import { styled } from "styled-components";
 import { Container } from "react-bootstrap";
 
+export const TitleBox = styled(Container)`
+  padding : 0 3rem;
+  display : flex;
+  justify-content : space-between;
+  margin-bottom : 7rem;
+
+  &>div{  width : 40%;  }
+  &>div:last-child{  text-align : right;  }
+
+  @media (max-width : 768px){
+    flex-direction : column;
+    
+    &>div{  width : 100%;  }
+    &>div:last-child{  
+      text-align : left;
+      margin-top : 3rem;
+    }
+  }
+`;
+
+export const Title = styled.h2`
+  font-size : 2.5rem;
+  margin-bottom : 1.5rem;
+`;
+
+export const Description = styled.p`
+  font-size : 1.2rem;
+  line-height : 1.7rem;
+`;
+
 // 프로필  Box 
 export const Profile = styled(Container)`
   display : grid;
