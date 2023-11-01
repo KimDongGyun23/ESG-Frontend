09c71c9d0f39710fa8fd4d50bf06b35955abed77
Chaerim0626 <dasapcr@gmail.com>
2023-11-01 18:18:42 +0900

    🐛 회원가입 오류 수정 및 서버 연결 구현

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import Header from './components/Layouts/Header';
-import Footer from './components/Layouts/Footer';
-import MainPage from './pages/MainPage/MainPage';
-import LoginPage from './pages/LoginPage/LoginPage';
-import ErrorPage from './pages/ErrorPage/ErrorPage';
-import IntroducePage from './pages/IntroducePage/IntroducePage';
-import LearningPage from './pages/LearningPage/LearningPage';
-import { Route, Routes } from 'react-router-dom';
-import Concept from './pages/LearningPage/Concept';
-import Importance from './pages/LearningPage/Importance';
-import Quiz from './pages/LearningPage/Quiz';
-import ForumPage from './pages/ForumPage/ForumPage';
-import Questions from './pages/ForumPage/Questions';
+import Header from "./components/Layouts/Header";
+import Footer from "./components/Layouts/Footer";
+import MainPage from "./pages/MainPage/MainPage";
+import LoginPage from "./pages/UserPage/LoginPage";
+import SignUpPage from "./pages/UserPage/SignUpPage";
+import ErrorPage from "./pages/ErrorPage/ErrorPage";
+import IntroducePage from "./pages/IntroducePage/IntroducePage";
+import LearningPage from "./pages/LearningPage/LearningPage";
+import { Route, Routes } from "react-router-dom";
+import Concept from "./pages/LearningPage/Concept";
+import Importance from "./pages/LearningPage/Importance";
+import Quiz from "./pages/LearningPage/Quiz";
+import ForumPage from "./pages/ForumPage/ForumPage";
+import Questions from "./pages/ForumPage/Questions";
 
 // 부트스트랩, 스타일 컴포넌트, 리액트 라우터 돔 설치
 function App() {
@@ -18,27 +19,28 @@ function App() {
     <div className="App">
       <Header />
       <Routes>
-        <Route path='/' element = { <MainPage /> } />
-        <Route path='/introduce' element = { <IntroducePage /> } />
-        <Route path='/login' element = { <LoginPage /> } />
-        
-        <Route path='/learning' element = { <LearningPage /> }>
-          <Route index element = { <Concept /> } />
-          <Route path='/learning/concept' element = { <Concept /> } />
-          <Route path='/learning/importance' element = { <Importance /> } />
-          <Route path='/learning/quiz' element = { <Quiz /> } />
+        <Route path="/" element={<MainPage />} />
+        <Route path="/introduce" element={<IntroducePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+
+        <Route path="/learning" element={<LearningPage />}>
+          <Route index element={<Concept />} />
+          <Route path="/learning/concept" element={<Concept />} />
+          <Route path="/learning/importance" element={<Importance />} />
+          <Route path="/learning/quiz" element={<Quiz />} />
         </Route>
-        
-        <Route path='/forum' element = { <ForumPage /> }>
-        <Route index element = { <Questions /> } />
-          <Route path='/forum/questions' element = { <Questions /> } />
-          <Route path='/forum/environment' element = { "environment" } />
-          <Route path='/forum/social' element = { "social" } />
-          <Route path='/forum/governance' element = { "governance" } />
-          <Route path='/forum/tags' element = { "tags" } />
+
+        <Route path="/forum" element={<ForumPage />}>
+          <Route index element={<Questions />} />
+          <Route path="/forum/questions" element={<Questions />} />
+          <Route path="/forum/environment" element={"environment"} />
+          <Route path="/forum/social" element={"social"} />
+          <Route path="/forum/governance" element={"governance"} />
+          <Route path="/forum/tags" element={"tags"} />
         </Route>
-        
-        <Route path="*" element={ <ErrorPage /> } />
+
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
       <Footer />
     </div>
--- src/components/Form/Login.js
+++ src/components/Form/Login.js
@@ -2,69 +2,71 @@ import { Form } from "react-bootstrap";
 import * as S from "../../styles/Form/Form.style";
 
 import axios from "axios";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import Validation from "./LoginValidation";
 
-
 // 로그인 버튼 선택 시
-function Login(){
-
+function Login() {
+  const [isLogin, setIsLogin] = useState(false);
   const [values, setValues] = useState({
-    email: '',
-    password: ''
+    email: "",
+    password: "",
   });
 
-
   const navigate = useNavigate();
-
   const [errors, setErrors] = useState({});
 
   const handleInput = (event) => {
-  const { name, value } = event.target;
-  console.log('Input changed:', name, value);
-  setValues((prev) => ({ ...prev, [name]: value }));
-};
+    const { name, value } = event.target;
+    console.log("Input changed:", name, value);
+    setValues((prev) => ({ ...prev, [name]: value }));
+  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     const validationErrors = Validation(values);
     setErrors(validationErrors);
 
-    console.log('Entered:', values);
+    console.log("입력 값:", values);
+
+    // 서버로 데이터 전송
+    const dataToSend = {
+      email: values.email,
+      password: values.password,
+    };
+
+    console.log("입력값 출력", dataToSend);
 
     // 이메일과 비밀번호 입력 조건 만족 시, 로그인 진행
     if (!validationErrors.email && !validationErrors.password) {
-        try {
-            const res = await axios.post('http://localhost:8081/login', values);
-            console.log('Got:', res.data);
-            if (res.data === "Success") { // backend에서 보낸 Success 확인
-                navigate('/');
-            } else {
-                alert("No record existed");
-            }
-        } catch (err) {
-            console.log(err);
-        }
+      axios
+        .post("/login", dataToSend)
+        .then((res) => {
+          console.log("로그인 성공 및 로그인 정보:", res.data);
+          setIsLogin(true); // 로그인 성공 시 isLogin을 true로 설정
+          navigate("/"); // 성공 시 새로고침
+        })
+        .catch((err) => {
+          console.log("로그인 실패 에러: ", err);
+        });
     }
-  }
+  };
 
-/**
- * < Login >
- * Form.Group ( 이메일, 비밀번호 )
- * axios를 통한 통신 ( 차후 수정 )
- */
-  return(
+  /**
+   * < Login >
+   * Form.Group ( 이메일, 비밀번호 )
+   */
+  return (
     <S.FormContainer>
-
       <Form onSubmit={handleSubmit}>
         <Form.Group controlId="formEmail">
           <Form.Label>이메일 주소</Form.Label>
           <S.InputBox>
-            <S.Input 
-              type="email" 
-              placeholder="Enter your Email Address" 
-              name='email' 
+            <S.Input
+              type="email"
+              placeholder="Enter your Email Address"
+              name="email"
               onChange={handleInput}
             />
           </S.InputBox>
@@ -74,9 +76,9 @@ function Login(){
           <Form.Label>비밀번호</Form.Label>
           <S.InputBox>
             <S.Input
-              type="password" 
-              placeholder="Enter Your Password" 
-              name='password' 
+              type="password"
+              placeholder="Enter Your Password"
+              name="password"
               onChange={handleInput}
             />
             <i className="fa-solid fa-eye-slash"></i>
@@ -85,20 +87,17 @@ function Login(){
 
         <S.LoginHelper>
           <Form.Check
-              // type=""
-              // id=""
-              label="계정 기억하기"
+            // type=""
+            // id=""
+            label="계정 기억하기"
           />
           <a href="*">비밀번호를 잊으셨나요?</a>
         </S.LoginHelper>
 
         <S.BtnLogin type="submit">로그인</S.BtnLogin>
       </Form>
-
-
-
     </S.FormContainer>
-  )
-};
+  );
+}
 
-export default Login; 
+export default Login;
